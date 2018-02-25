ad64edfaf0f161ce5733f0c07eade4473dddac5c
milad <ebrahimpourmilad74@gmail.com>
2018-02-25 19:24:40 +0330

    #4 Style in P

---
+++ mochaTests/testObject.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var _ = require('lodash');
+describe('testAll', function () {
+    it('objectTest', function (done) {
+        var style = {
+            fontFamily: 'B Nazanin',
+            fontSize: '11',
+            color: 'red',
+            bold: false,
+            direction: 'rtl',
+            align: 'right',
+            background: 'blue'
+        };
+        done();
+    }); // it
+}); //  describe
+//# sourceMappingURL=testObject.js.map
---
+++ mochaTests/testObject.ts
@@ -0,0 +1,21 @@
+import {} from 'mocha';
+let _ = require('lodash');
+
+describe('testAll', function(){
+
+    it('objectTest', function(done){
+        let style = {
+            fontFamily:'B Nazanin',
+            fontSize : '11',
+            color:'red',
+            bold: false,
+            direction: 'rtl',
+            align : 'right',
+            background: 'blue'
+        };
+
+
+        done()
+    });// it
+
+});//  describe
