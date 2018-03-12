05e6150f10cd7999e518aab68d1ed05c11b0ff2b
milad <ebrahimpourmilad74@gmail.com>
2018-03-12 09:14:47 -0700

    #18 test_Merge_Tbl_docx

--- UnitTests/test_Merge_Tbl_docx.js
+++ UnitTests/test_Merge_Tbl_docx.js
@@ -1,1 +1,39 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var assert_1 = require("assert");
+var Docx_1 = require("../classes/Docx");
+describe('docx', function () {
+    it('Merge_Tbl_docx', function (done) {
+        try {
+            var data = [
+                { x: 1, y: 0, value: '', mergeRow: '2', mergeCol: '3' },
+                { x: 1, y: 1, value: 'سال 1390', mergeRow: '', mergeCol: '' },
+                { x: 1, y: 2, value: 'سال1391', mergeRow: '', mergeCol: '' },
+                { x: 2, y: 0, value: 'کل', mergeRow: '', mergeCol: '' },
+                { x: 2, y: 1, value: '21545288', mergeRow: '', mergeCol: '' },
+                { x: 2, y: 2, value: '85487525', mergeRow: '', mergeCol: '' },
+                { x: 3, y: 0, value: 'البرز', mergeRow: '', mergeCol: '' },
+                { x: 3, y: 1, value: '2521', mergeRow: '', mergeCol: '' },
+                { x: 3, y: 2, value: '5485', mergeRow: '', mergeCol: '' },
+                { x: 4, y: 0, value: 'بندرعباس', mergeRow: '', mergeCol: '' },
+                { x: 4, y: 1, value: '145214', mergeRow: '', mergeCol: '' },
+                { x: 4, y: 2, value: '2255', mergeRow: '', mergeCol: '' }
+            ]; // data
+            var docx = new Docx_1.Docx('test_Merge_Tbl.docx', 'outputUnitTest/');
+            docx.createTable(data);
+            var out = docx.generate();
+            if (out == false) {
+                assert_1.fail("Don't create File of docx");
+            }
+            else {
+                console.log("Create File of Docx");
+                done();
+            }
+        }
+        catch (e) {
+            assert_1.fail(e);
+            done();
+        }
+    }); // it Merge&OrginalTbl_docx
+});
 //# sourceMappingURL=test_Merge_Tbl_docx.js.map
